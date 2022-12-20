41f92bac3f6ceed0c3f779efb0799ea947584027
daniel fligg <danielfligg@yahoo.de>
2022-12-20 20:29:13 +0100

    cross debugging vscode add

--- utils/copybin.sh
+++ utils/copybin.sh
@@ -3,7 +3,6 @@
 cd $1
 ip=$2
 user=$3
-pass=$4
 
 pwd
 
@@ -13,8 +12,9 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-rsync -arv --update --rsh="sshpass -p $pass ssh -o StrictHostKeyChecking=no -l $user" include/* $ip:/usr/local/include
-rsync -arv --update  --rsh="sshpass -p $pass ssh -o StrictHostKeyChecking=no -l $user" bin/* $ip:/usr/local/bin
-rsync -arv --update  --rsh="sshpass -p $pass ssh -o StrictHostKeyChecking=no -l $user" lib/* $ip:/usr/local/lib
-rsync -arv --update  --rsh="sshpass -p $pass ssh -o StrictHostKeyChecking=no -l $user" etc/* $ip:/usr/local/etc
+scp -r include/* $user@$ip:/usr/local/include
+scp -r bin/*     $user@$ip:/usr/local/bin
+scp -r lib/*     $user@$ip:/usr/local/lib
+scp -r etc/*     $user@$ip:/usr/local/etc
+
 echo "install success on $ip!"
